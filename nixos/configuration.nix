da4bb07f9bda41aca27d092e83c9da49360fc789
greed-d <deekshit.bti@gmail.com>
2024-09-14 11:08:39 +0545

    `chore` : add sessionVariables

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -191,10 +191,11 @@
       inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
   #
-  # environment.sessionVariables = {
-  #   NIXOS_OZONE_WL = 1;
-  # };
-  #
+  environment.sessionVariables = {
+    EDITOR = "nvim";
+    BROWSER = "Zen-alpha";
+    TERMINAL = "kitty";
+  };
 
   xdg.portal = {
     enable = true;
@@ -204,7 +205,7 @@
     ];
 
     config.common = {
-      default = [ "gtk" ];
+      default = "hyprland;gtk";
       "org.freedesktop.impl.portal.Screencast" = [ pkgs.hyprland ];
     };
 
