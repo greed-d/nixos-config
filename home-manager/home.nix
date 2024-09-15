adaa0beee98ed4e29a8a52cfcfc54c713c6899fe
greed-d <deekshit.bti@gmail.com>
2024-09-15 23:25:20 +0545

    `testing still going on`

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -81,12 +81,11 @@
     starship
     zoxide
     webkitgtk
-    tmux
-    hyprpanel
-    hyprpicker
-    hypridle
-    hyprpaper
-    hyprlock
+    # hyprpanel
+    # hyprpicker
+    # hypridle
+    # hyprpaper
+    # hyprlock
     grim
     slurp
     wl-clip-persist
@@ -135,8 +134,50 @@
       package = pkgs.tokyonight-gtk-theme;
       name = "Tokyonight-Dark";
     };
+
   };
 
+  programs.tmux = {
+    enable = true;
+    prefix = "C-a";
+    plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavour "mocha"
+          set -g @catppuccin_window_tabs_enabled on
+          set -g @catppuccin_date_time "%H:%M"
+        '';
+      }
+      {
+        plugin = tmuxPlugins.resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-vim "session"
+          set -g @resurrect-strategy-nvim "session"
+          set -g @resurrect-capture-pane-contents "on"
+        '';
+      }
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+          set -g @continuum-restore "on"
+          set -g @continuum-boot "on"
+          set -g @continuum-save-interval "10"
+        '';
+      }
+    ];
+    extraConfig = ''
+      # Fix color issues with terminal
+      set -g default-terminal "$TERM"
+      set-option -ag terminal-overrides ",$TERM:RGB"
+
+      # Bring Back Transparency
+      set -g pane-active-border-style bg=color0
+      set -g pane-border-style bg=color0
+      set-window-option -g window-active-style bg=terminal
+      set-window-option -g window-style bg=color0
+    '';
+  };
   # Enable home-manager and git
   programs.home-manager.enable = true;
   # programs.home-manager.backupFileExtension = "backup";
