a1fdf6a4005d27eb3a1c939e6e5026ef18b3e167
greed-d <deekshit.bti@gmail.com>
2024-09-16 19:27:08 +0545

    `add` : tokyo-night sddm theme

--- nixos/tokyo-night-sddm/default.nix
+++ nixos/tokyo-night-sddm/default.nix
@@ -13,10 +13,10 @@ stdenvNoCC.mkDerivation rec {
   version = "1..0";
   dontBuild = true;
   src = fetchFromGitHub {
-    owner = "siddrs";
+    owner = "greed-d";
     repo = pname;
-    rev = "320c8e74ade1e94f640708eee0b9a75a395697c6";
-    sha256 = "1gf074ypgc4r8pgljd8lydy0l5fajrl2pi2avn5ivacz4z7ma595";
+    rev = "44967486eb67abe8956c9d33abf83ef601dbccce";
+    sha256 = "0z9xxqqbby50xsdr8i2j4ls1dd9s3hqmlpv1qcbc5mgfbakc5slm";
   };
   nativeBuildInputs = [
     wrapQtAppsHook
@@ -32,8 +32,6 @@ stdenvNoCC.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/share/sddm/themes
     cp -aR $src $out/share/sddm/themes/tokyo-night-sddm
-    cd $out/share/sddm/themes/tokyo-night-sddm
-    vim -c '%s/win11\.png/tokyocity.png/g | wq' inputfile
   '';
 
 }
